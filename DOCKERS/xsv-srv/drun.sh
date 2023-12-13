20e2bee1857f4c87cabce80e9124ae8ce90cb817
bletekk <bletekk@gmail.com>
2023-12-13 15:55:34 +0100

    Update drun.sh
    single backslash issue resolved

--- DOCKERS/xsv-srv/drun.sh
+++ DOCKERS/xsv-srv/drun.sh
@@ -2,5 +2,5 @@
 
 MY_HOME=$(pwd)
 
-docker run -d -p 19090:19090 --name xsv-srv -h xsv-srv -v $MY_HOME\lxaip:/opt/lxaip xsv-srv:1.0.0
+docker run -d -p 19090:19090 --name xsv-srv -h xsv-srv -v $MY_HOME/lxaip:/opt/lxaip xsv-srv:1.0.0
 
