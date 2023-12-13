922a1fda0bc67cb39bdd96c159cb9a165eedbb9d
bletekk <bletekk@gmail.com>
2023-12-13 15:53:08 +0100

    Update drun.sh
    a single backslash issue by volume mapping resolved

--- DOCKERS/erv-srv/drun.sh
+++ DOCKERS/erv-srv/drun.sh
@@ -2,5 +2,5 @@
 
 MY_HOME=$(pwd)
 
-docker run -d -p 9090:9090 --name erv-srv -h erv-srv -v $MY_HOME\lxaip:/opt/lxaip erv-srv:1.0.0
+docker run -d -p 9090:9090 --name erv-srv -h erv-srv -v $MY_HOME/lxaip:/opt/lxaip erv-srv:1.0.0
 
